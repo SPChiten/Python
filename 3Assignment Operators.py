c33235e4f6d95fcabcde37264739cfed186823ec
Chiten <95770092+SPChiten@users.noreply.github.com>
2023-12-03 11:07:26 -0500

    Add files via upload
    Assignment Operators

---
+++ 3Assignment Operators.py
@@ -0,0 +1,24 @@
+a = 11
+b = 7
+c = 0
+
+c = a + b
+print(c) # output 18
+
+c += a # meaning c = c + a
+print(c) # output 29
+
+c *= a
+print(c) # output 319
+
+c /= a
+print(c) # output 29
+
+c %= a
+print(c) # output 7
+
+c **= a
+print(c) # output 1977326743.0
+
+c //= a
+print(c) # output 179756976.0
