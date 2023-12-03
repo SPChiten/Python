953be5f5bc05106db06ef9983c0dbc236339181e
Chiten <95770092+SPChiten@users.noreply.github.com>
2023-12-03 11:05:30 -0500

    Add files via upload
    Arithmatic Operators

---
+++ 2Arithmatic Operators.py
@@ -0,0 +1,28 @@
+a = 49
+b = 30
+c = 0
+
+c = a + b
+print(c) # output 79
+
+c = a - b
+print (c) # output 19
+
+c = a * b
+print(c) # output 1470
+
+c = a / b
+print(c) # output 1.6333333
+
+c = a % b
+print(c) # output 19
+
+a = 3
+b = 2
+c = a**b
+print(c) # output 9
+
+a = 7
+b = 5
+c = a // b
+print(c) # output 1
